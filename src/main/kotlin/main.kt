6463c0beb610934eabd393f1e8c3ec737eb6aac4
Roman Ivanov <roman_ivanov85@internet.ru>
2022-11-01 22:00:38 +0300

    add picture and README

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,12 @@
 package ru.netology
 
 fun main() {
+    val likes = 115
 
+    val ending = if (likes % 100 == 11) {
+        "людям"
+    } else if (likes % 10 == 1) "человеку" else {
+        "людям"
+    }
+    println("Понравилось $likes $ending")
 }
